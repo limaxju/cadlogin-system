71213361d9294f728d7f2d06f6aa776495d62640
limaxju <eduarda.julia@alunoescolassociais.org.br>
2024-09-11 09:32:22 -0300

    controladores de arquivo, função para realizar login

---
+++ controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+//Requer arquivo o arquivo user que contem o model user com as funções de manipulação de dados de usuario
+require_once 'models/user.php'
+
+class AuthController
+{
+    //Função para realizar o login
+    public function login()
+    {
+        //Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+
+    }
+}
+
+?>
--- routes.php
+++ routes.php
@@ -7,5 +7,16 @@ require 'controllers/DashboardController.php';//inclui o controlador de dashboar
 
 //Cria instancias dos controladores para utilizar seus métodos
 $authController = new AuthController(); //instancia o controlador de autenticação
+$userController = new UserController(); //instancia o controlador de usuarios
+$dashboardController = new DashboardController(); //instancia o controlador de dashboard
+
+//Coleta a ação de URL, se não houver ação definida, usa 'login' como padrão
+
+$acao = $_GET['acao'] ?? 'login'; //Usa operador de coalescencia nula (??) para definir 'login' se 'action' não estiver presente
+
+switch($action){
+    case 'login':
+        $authController->login();//chama o método de login do controlador de autenticação
+}
 
 ?>
