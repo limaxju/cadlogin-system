1ebba253d389b5e632b261fc03c3a22d4964269a
limaxju <eduarda.julia@alunoescolassociais.org.br>
2024-09-24 08:27:20 -0300

    verificação da requisição, armazenamento do id usuario

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,8 +8,27 @@ class AuthController
     public function login()
     {
         //Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            //obtem os dados do formulário
+            $email = $_POST['email'];
+            $senha = $_POST['senha'];
+
+            //chama a função login do model user para verificar se os dados estão corretos
+            $user = User::findByEmail();
+
+            if($user && password_verify($senha,$user['senha'])){//verifica se a senha corresponde a um hash
+
+                session_start();
+                //armazena na sessão o id do usuario que esta logado e seu perfil
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['perfil'] = $user['perfil'];
+                header('');
+
+
+            }
+    
 
     }
 }
-
+}
 ?>
